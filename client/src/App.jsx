5759b5eb9c17d3c0783dd26b73d77387d41fc90c
AlexVovan <alehnovich.vova@gmail.com>
2022-01-26 15:17:17 +0100

    add reducer for user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,14 @@ import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Success from "./pages/Success";
+import { useSelector } from "react-redux";
 
 
 const App = () => {
-  const user = true;
+  const user = useSelector((state) => state.user.currentUser);
   return (
     <Router>
       <Switch>
-
         <Route exact path="/">
           <Home />
         </Route>
@@ -36,7 +36,6 @@ const App = () => {
           {user ? <Redirect to="/"/> : <Register/>}
           <Register />
         </Route>
-
       </Switch>
     </Router>
   );
