d9c0705967c01f8b5625d995c73162bba4b6170c
AlexVovan <alehnovich.vova@gmail.com>
2022-01-28 18:52:23 +0100

    add admin panel

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './app.css';
+import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,67 @@
+import './widgetSm.css';
+import { Visibility } from '@material-ui/icons';
+
+export default function WidgetSm() {
+    return (
+        <div className="widgetSm">
+            <span className="widgetSmTitle">New Join Members</span>
+            <ul className="widgetSmList">
+                <li className="widgetSmListItem">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9Q_UAvVx3bc1EjsKuIWbTmAggl7hCkKmu5w&usqp=CAU" alt="" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9Q_UAvVx3bc1EjsKuIWbTmAggl7hCkKmu5w&usqp=CAU" alt="" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9Q_UAvVx3bc1EjsKuIWbTmAggl7hCkKmu5w&usqp=CAU" alt="" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9Q_UAvVx3bc1EjsKuIWbTmAggl7hCkKmu5w&usqp=CAU" alt="" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9Q_UAvVx3bc1EjsKuIWbTmAggl7hCkKmu5w&usqp=CAU" alt="" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
+}
