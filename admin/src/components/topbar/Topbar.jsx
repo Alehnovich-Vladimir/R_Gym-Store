0bdb52686e52207aea18ede3156a0bee5ed8a838
AlexVovan <alehnovich.vova@gmail.com>
2022-01-30 00:53:51 +0100

    add newProduct page

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar() {
         <div className="topbar">
             <div className="topbarWrapper">
                 <div className="topLeft">
-                    <span className="logo">Vovaadmin</span>
+                    <span className="logo">Admin</span>
                 </div>
                 <div className="topRight">
                     <div className="topbarIconContainer">
