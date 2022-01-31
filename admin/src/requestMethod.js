92132c5074d741082d582f93e28a29005de652e3
AlexVovan <alehnovich.vova@gmail.com>
2022-01-31 10:36:53 +0100

    bug

---
+++ admin/src/requestMethod.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const BASE_URL = "http://localhost:5000/api/";
+const TOKEN = "";
+
+console.log(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).currentUser).accessToken);
+
+export const publicRequest = axios.create({
+    baseURL: BASE_URL,
+});
+
+export const userRequest = axios.create({
+    baseURL: BASE_URL,
+    header: { token: `Bearer ${TOKEN}` },
+});
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTk3Nzg0MWQyNDkzN2FkZWJiNmVjNyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0Mjg5NDY3OCwiZXhwIjoxNjQzMTUzODc4fQ.HfAvvHS_z8YM6j766PSxabX2Tw8A1wQ5A_wAhSiD_4E
